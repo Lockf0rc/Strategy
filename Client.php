2081a50ade60a5b92ad29e7eb5441265e3c2a97c
Miguel Escamilla <metuxxman@gmail.com>
2015-03-26 02:29:52 -0700

    add Client file

---
+++ Client.php
@@ -0,0 +1,13 @@
+<?php
+/*STRATEGY PATTERN*/
+class Client
+{
+	public function inserData(){}
+	public function findData(){}
+	public function showAll(){}
+	public function changeData(){}
+	public function killer(){}
+}
+
+
+?>
